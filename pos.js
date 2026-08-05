48ec9b956ca5af1dd63d1067aa3f009f95d1905a
rkdeveloperstudio-cloud <rkdeveloperstudio@gmail.com>
2026-08-05 22:11:20 +0400

    Update pos.js

--- pos.js
+++ pos.js
@@ -949,44 +949,6 @@ reprintLastBill;
 
 
 
-// ===============================
-// PRINTER SERVICE CONTROLLER
-// ===============================
-
-const PrinterService =
-{
-
-mode: localStorage.getItem("printMode") || "SHARE",
-
-
-setMode(value)
-{
-
-    this.mode=value;
-
-},
-
-
-async print(receipt)
-{
-
-    if(this.mode==="SHARE")
-    {
-
-        await MobilePrinter.print(receipt);
-
-    }
-    else if(this.mode==="BROWSER")
-    {
-
-        await BrowserPrinter.print(receipt);
-
-    }
-
-}
-
-
-};
 
 
 function loadPrinterSettings()
